146c07809f932ce7834cead06b460256f6b2f6a1
Daniela Codes <daniela.antonio.430@gmail.com>
2023-10-12 16:47:57 -0400

    Simple-JS-App
    follow along notes from Reading 1.1

--- simple-js-app/js/script.js
+++ simple-js-app/js/script.js
@@ -1,1 +1,28 @@
-alert('Hello world'); 
+// Learning about variables // 
+/* let myName = 'Bob';
+document.write(myName) ;
+myName = 'John Doe';
+document.write(myName) ;  */
+
+// Using opertators // 
+// Add two numbers together // 
+/* let simpleAddition = 2 + 2;
+   document.write (simpleAddition) ; */ // Output = 4 //
+
+//Multiply //
+/* let size = 100;
+let doubleSize = size * 2;
+document.write (doubleSize) ; */ // Output = 200 //
+
+// Using multiple variables // 
+let size = 100;
+let doubleSize = size * 2; 
+let minSize = (doubleSize * 2) - (size / 2) ; 
+document.write(minSize) ;  // Output = 350 // 
+
+
+
+
+
+
+
