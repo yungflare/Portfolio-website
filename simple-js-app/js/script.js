c7a7d154634455947bceea1a3595c59b1a10e65e
Daniela Codes <daniela.antonio.430@gmail.com>
2023-10-13 11:22:43 -0400

    NOTES FOR 1.2
    Getting familiar with strings
    How to use quotes
    Using more than one variable
    Using dev tools -> Console, writing lines of code to do math
    get familiar with how to write lines that are more of a literal output.

--- simple-js-app/js/script.js
+++ simple-js-app/js/script.js
@@ -18,9 +18,30 @@ document.write (doubleSize) ; */ // Output = 200 //
 let size = 100;
 let doubleSize = size * 2; 
 let minSize = (doubleSize * 2) - (size / 2) ; 
-document.write(minSize) ;  // Output = 350 // 
+// document.write(minSize) ;  Output = 350 // 
 
+/* Primitive Data Types : 
+      STRINGS */
 
+let text1 = ' This is a text! ';
+let text2 = " This uses double quotes." ; 
+// document.write(text2) ;    Output = same for both strings (reg text) // 
+
+let escapedText = "He said: \"Yes!!\", that's for sure. ";
+let escapedText2 = 'He said: "Yes!", that\'s for sure.' ; 
+// document.write(escapedText) ;   Output = same for both (reg text) // 
+
+// Contenating Strings // 
+
+let name = 'John' ;        // variable = name & value = john // 
+// document.write('My name is ' + name + ' and I am ready to start!') ;   Output = sentence written correctly. space after is and before and is very important // 
+
+// Combining strings with numbers //
+
+let name1 = 'Durk' ; 
+let age = 42;
+document.write('My name is ' + name1 + ' and I\'m ' + age + ' years old!') ; 
+console.log('My name is ' + name + ' and I\'m ' + age + ' years old!' ) ; 
 
 
 
